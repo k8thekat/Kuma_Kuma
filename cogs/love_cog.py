4c9560b6693e5f21ba8b51903f4c719d235177dc
k8thekat <cadwalladerkatelynn@gmail.com>
2023-06-08 22:14:28 -0700

    Needs more testing; commands all fleshed out. Co-authored-by: Leonardo <leonardocavenago05@gmail.com>

--- cogs/love_cog.py
+++ cogs/love_cog.py
@@ -23,46 +23,90 @@ from __future__ import annotations
 
 import datetime
 from datetime import timezone
-from dis import disco
+from pydoc import describe
+import sqlite3
 import discord
 import os
 import logging
 from dataclasses import dataclass
+from pprint import pprint
 
-from discord import Interaction, app_commands
+from discord import Embed, Interaction, app_commands
 from discord.app_commands import Choice
+from discord.colour import Colour
 from discord.ext import commands, tasks
 import asqlite
 
-from typing import Union
+from typing import Any, List, Optional, Union
 
 from kuma_kuma import Kuma_Kuma
 
 DB_FILENAME = "lovers.sqlite"
 
 LOVERS_SETUP_SQL = """
-CREATE TABLE IF NOT EXISTS lovers(
+CREATE TABLE IF NOT EXISTS lovers (
     name TEXT NOT NULL,
     discord_id BIGINT NOT NULL,
-)"""
+    PRIMARY KEY(discord_id)
+)
+"""
 
 PARTNERS_SETUP_SQL = """
-CREATE TABLE IF NOT EXISTS partners(
+CREATE TABLE IF NOT EXISTS partners (
     lovers_id INT NOT NULL,
     partner_id INT NOT NULL,
-    FOREIGN KEY (lovers_id) references lovers(discord_id)
+    FOREIGN KEY (lovers_id) references lovers(discord_id),
     FOREIGN KEY (partner_id) references lovers(discord_id)
-)"""
+    PRIMARY KEY(lovers_id, partner_id)
+)
+"""
 
 KINKS_SETUP_SQL = """
-CREATE TABLE IF NOT EXISTS kinks(
+CREATE TABLE IF NOT EXISTS kinks (
     lovers_id INT NOT NULL,
     name TEXT NOT NULL,
     description TEXT,
     FOREIGN KEY (lovers_id) references lovers(discord_id)
-)"""
+    PRIMARY KEY(lovers_id, name)
+)
+"""
 
-_logger = logging.getLogger(__name__)
+_logger = logging.getLogger()
+
+
+class LoverEmbed(discord.Embed):
+    @classmethod
+    async def create(cls, *, color: int | Colour | None = None, title: Any | None = None, timestamp: datetime.datetime | None = None, lover: LoverEntry, interaction: discord.Interaction):
+        self = cls(color=color, title=title, timestamp=timestamp)
+
+        assert interaction.guild
+        partners = await lover.list_partners()
+        if partners == None:
+            partners = "None"
+        self.add_field(name="**Partners**", value=[interaction.guild.get_member(int(x)) for x in partners])
+
+        kinks = await lover.list_kinks()
+        if kinks == None:
+            kinks = "None"
+        self.add_field(name="**Kinks**", value=kinks, inline=True)
+
+        return self
+
+
+class PartnerEmbed(discord.Embed):
+    @classmethod
+    async def create(cls, *, color: int | Colour | None = None, title: Any | None = None, timestamp: datetime.datetime | None = None, partner: discord.Member):
+        self = cls(color=color, title=title, timestamp=timestamp)
+        self.set_thumbnail(url=None if partner.avatar == None else partner.avatar.url)
+
+        lover: LoverEntry | None = await LoverEntry.get_or_none(discord_id=partner.id)
+        if lover is not None:
+            res = await lover.list_kinks()
+            if res is not None:
+                for entry in res:
+                    self.add_field(name=entry["name"], value=entry["description"])
+
+        return self
 
 
 @dataclass(slots=True)
@@ -86,7 +130,7 @@ class LoverEntry:
         async with asqlite.connect(DB_FILENAME) as db:
             async with db.cursor() as cur:
                 await cur.execute("""INSERT INTO lovers(name, discord_id) VALUES (?, ?)
-                ON CONFLICT(name, discord_id) DO NOTHING RETURNING *""", name, discord_id)
+                ON CONFLICT(discord_id) DO NOTHING RETURNING *""", name, discord_id)
                 res = await cur.fetchone()
                 await db.commit()
 
@@ -115,14 +159,21 @@ class LoverEntry:
         if lover == None:
             lover = await self.add_lover(name=partner_name, discord_id=partner_id)
 
-        else:
+        if lover != None:
             async with asqlite.connect(DB_FILENAME) as db:
                 async with db.cursor() as cur:
-                    await cur.execute("""INSERT INTO partners(lovers_id, partner_id) VALUES (?, ?)
-                    ON CONFLICT(lovers_id, partner_id) DO NOTHING RETURNING *""", lover.discord_id, partner_id)
-                    res = await cur.fetchone()
-                    await db.commit()
+                    # await cur.execute("""INSERT INTO partners(lovers_id, partner_id) VALUES (?, ?)
+                    # ON CONFLICT(lovers_id, partner_id) DO NOTHING RETURNING *""", lover.discord_id, partner_id)
+                    try:
+                        await cur.execute("""INSERT INTO partners(lovers_id, partner_id) VALUES (?, ?)""", self.discord_id, partner_id)
 
+                    except sqlite3.IntegrityError as err:
+                        if type(err.args[0]) == str and err.args[0].lower() == "unique constraint failed: partners.lovers_id, partners.partner_id":
+                            return None
+
+                    # res = await cur.fetchone()
+                    await db.commit()
+                    return lover
         # return lover
 
     async def remove_partner(self, partner_id: int) -> None | int:
@@ -144,7 +195,8 @@ class LoverEntry:
             async with db.cursor() as cur:
                 await cur.execute("""SELECT partner_id FROM partners WHERE lovers_id = ?""", self.discord_id)
                 res = await cur.fetchall()
-
+                if res:
+                    res = [entry["partner_id"] for entry in res]
                 return res if not None else None
 
     async def add_kink(self, name: str, description: Union[str, None] = None) -> str | None:
@@ -165,6 +217,16 @@ class LoverEntry:
                 await db.commit()
 
                 return cur.get_cursor().rowcount
+
+    # async def update_kink(self, name: str, new_name: str | None = None, new_description: str | None = None) -> int | None:
+    #     async with asqlite.connect(DB_FILENAME) as db:
+    #         async with db.cursor() as cur:
+    #             await cur.execute("""SELECT * FROM kinks WHERE name = ?""", name)
+    #             res = await cur.fetchone()
+    #             if res is not None:
+    #                 name = name if new_name == None else new_name
+    #                 description = res["description"] if new_description == None else new_description
+    #                 await cur.execute("""UPDATE kinks SET name = ?, description = ? WHERE name = ?""", name, description)
 
     async def list_kinks(self) -> list | None:
         async with asqlite.connect(DB_FILENAME) as db:
@@ -194,9 +256,15 @@ class Love(commands.Cog):
         self._logger = logging.getLogger()
         self._logger.info(f'**SUCCESS** Initializing {self._name} ')
 
-    async def partner_autocomplete(self, interaction: discord.Interaction, current: str) -> list[Choice[str]]:
+    async def cog_load(self) -> None:
+        async with asqlite.connect(DB_FILENAME) as db:
+            await db.execute(LOVERS_SETUP_SQL)
+            await db.execute(PARTNERS_SETUP_SQL)
+            await db.execute(KINKS_SETUP_SQL)
+
+    async def partner_autocomplete(self, interaction: discord.Interaction, current: str) -> List[app_commands.Choice]:
         assert interaction.guild
-        res: list[Choice] = []
+        res: list[Choice] = [app_commands.Choice(name="No Entries Found...", value='x' * 100)]
         choice_list: list[discord.Member] = []
 
         lover: LoverEntry | None = await LoverEntry.get_or_none(discord_id=interaction.user.id)
@@ -207,12 +275,32 @@ class Love(commands.Cog):
                 return res
             else:
                 for id in partners:
-                    member: discord.Member | None = interaction.guild.get_member(id["partner_id"])
+                    # print(type(id["partner_id"]))
+                    member_id = id["partner_id"]
+                    member: discord.Member | None = interaction.guild.get_member(member_id)
                     if member is not None:
                         # Choice[name= member.name, value= member.id]
                         choice_list.append(member)
 
-                return [Choice(name=member.name, value=str(member.id)) for member in choice_list if current.lower() in member.name.lower()]
+            return [app_commands.Choice(name=member.name, value=str(member.id)) for member in choice_list if current.lower() in member.name.lower()]
+        return res
+
+    async def kinks_autocomplete(self, interaction: discord.Interaction, current: str):
+        assert interaction.guild
+        res: list[Choice] = []
+        choice_list: list[str] = []
+
+        lover: LoverEntry | None = await LoverEntry.get_or_none(discord_id=interaction.user.id)
+        if lover is not None:
+            kinks = await lover.list_kinks()
+
+            if kinks is None:
+                return res
+            else:
+                for entry in kinks:
+                    choice_list.append(entry["name"])
+
+                return [Choice(name=kink, value=kink) for kink in choice_list if current.lower() in kink.lower()]
         return res
 
     # @love_language.command(name="reroll")
@@ -223,30 +311,92 @@ class Love(commands.Cog):
     # async def love_message_interval(self, interaction: discord.Interaction) -> None:
     #     print()
 
-    @love_partner.command(name="add", description="Add a partner")
-    async def love_add_partner(self, interaction: discord.Interaction, partner: Union[discord.User, discord.Member]) -> None:
+    @love_language.command(name="user", description="Manage your user profile")
+    @app_commands.choices(action=[Choice(name="add", value="add"), Choice(name="delete", value="delete"), Choice(name="update", value="update")])
+    async def love_user_lover(self, interaction: discord.Interaction, action: Choice[str]):
         lover = await LoverEntry.get_or_none(discord_id=interaction.user.id)
 
+        if action.value == "add":
+            if lover is None:
+                await LoverEntry.add_lover(name=interaction.user.name, discord_id=interaction.user.id)
+                await interaction.response.send_message(content=f"Added *{interaction.user.name}*", ephemeral=True)
+            else:
+                await interaction.response.send_message(content=f"You are already a Lover user, get out there!", ephemeral=True)
+
+        if action.value == "delete":
+            if lover is not None:
+                await lover.delete_lover()
+                await interaction.response.send_message(content=f"We have removed {interaction.user.name} from the database, sad to see you go~", ephemeral=True)
+            else:
+                await interaction.response.send_message(content=f"I was unable to find a Lover by the name of `{interaction.user.name}`", ephemeral=True)
+
+        if action.value == "update":
+            if lover is not None:
+                await lover.update_lover(name=interaction.user.name)
+                await interaction.response.send_message(content=f"We updated your Lover name!", ephemeral=True)
+
+    # TODO - Verify operation
+    @love_partner.command(name="add", description="Add a partner")
+    async def love_add_partner(self, interaction: discord.Interaction, partner: discord.Member) -> None:
+
+        if partner.id == interaction.user.id:
+            return await interaction.response.send_message(content=f"You cannot add yourself as a partner... or can you?", ephemeral=True)
+
+        lover: LoverEntry | None = await LoverEntry.get_or_none(discord_id=interaction.user.id)
         if lover is not None:
-            await lover.add_partner(partner_name=partner.name, partner_id=partner.id)
+            result: LoverEntry | None = await lover.add_partner(partner_name=partner.name, partner_id=partner.id)
+            if result == None:
+                await interaction.response.send_message(content=f"Looks like **{partner.name}** is already your partner, get out there and have fun!", ephemeral=True)
+            if type(result) == LoverEntry:
+                # await interaction.response.send_message(content=f"You added **{partner.name}** as a partner.", ephemeral=True)
+                await interaction.response.send_message(embed=await LoverEmbed.create(color=interaction.user.color, timestamp=discord.utils.utcnow(), lover=lover, interaction=interaction), ephemeral=True)
 
-        await interaction.response.send_message(content=f"You added **{partner.name}** as a partner.")
+        else:
+            await interaction.response.send_message(content=f"It looks like `{interaction.user.name}` is not a *lover* user.", ephemeral=True)
 
+     # TODO - Verify operation
     @love_partner.command(name="remove", description="Remove a partner.")
     @app_commands.autocomplete(partner=partner_autocomplete)
-    async def love_remove_partner(self, interaction: discord.Interaction, partner: Union[discord.User, discord.Member]) -> None:
-        # TODO - Create a Discord User list of the Partner IDs of the lover from the DB.
+    async def love_remove_partner(self, interaction: discord.Interaction, partner: str) -> None:
+        if len(partner) == 100:
+            return await interaction.response.send_message(content='You don\'t have any partners! Why did you select that option?', ephemeral=True)
+
+        if not partner.isdigit():
+            return await interaction.response.send_message(content='You must choose from the options prompted to you.', ephemeral=True)
+
         lover = await LoverEntry.get_or_none(discord_id=interaction.user.id)
+        assert interaction.guild
 
         if lover is not None:
-            res: int | None = await lover.remove_partner(partner_id=partner.id)
+            res: int | None = await lover.remove_partner(partner_id=int(partner))
+            partner_name = interaction.guild.get_member(int(partner))
+            await interaction.response.send_message(content={f"We removed {res} as a partner," if res is not None else f"Unable to remove {partner_name}"}, ephemeral=True)
+        else:
+            await interaction.response.send_message(content=f"It looks like `{interaction.user.name}` is not a *lover* user.", ephemeral=True)
 
-        await interaction.response.send_message(content={f"We removed {res} partner." if res is not None else f"Unable to remove {partner.name}"})
-
+     # TODO - Verify operation
     @love_partner.command(name="list", description="Lists all your partners")
     async def love_list_partner(self, interaction: discord.Interaction) -> None:
-        print()
+        lover = await LoverEntry.get_or_none(discord_id=interaction.user.id)
+        assert interaction.guild
 
+        if lover is not None:
+            res: list | None = await lover.list_partners()
+            # TODO - Embed each user with Avatar icons?
+            if res is not None:
+                embeds = []
+                for x in res:
+                    partner: discord.Member | None = interaction.guild.get_member(int(x))
+                    if partner:
+                        partner_embed = await PartnerEmbed.create(color=partner.color, title=partner.name, timestamp=discord.utils.utcnow(), partner=partner)
+                        embeds.append(partner_embed)
+                await interaction.response.send_message(content=f"**{interaction.user.name}** Partner list..", embeds=embeds, ephemeral=True)
+            else:
+                await interaction.response.send_message(content=f"You have no partners, consider adding some? Get out there and flirt sexy~<3", ephemeral=True)
+        else:
+            await interaction.response.send_message(content=f"It looks like `{interaction.user.name}` is not a *lover* user.", ephemeral=True)
+
+     # TODO - Verify operation
     @love_kinks.command(name="add", description="Add a type of Kink/Play you like.")
     async def love_kink_add(self, interaction: discord.Interaction, kink: str, description: Union[str, None] = None) -> None:
 
@@ -254,12 +404,39 @@ class Love(commands.Cog):
         if lover is not None:
             await lover.add_kink(name=kink, description=description)
 
-            msg_content: str = f"Added {kink} to {interaction.user.name}"
+            msg_content: str = f"Added `{kink}` to **{interaction.user.name}**"
             if description is not None:
-                msg_content += "\n **Description**:" + description
+                msg_content += "\n **Description**: " + description
 
-            await interaction.response.send_message(content=msg_content)
+            await interaction.response.send_message(content=msg_content, ephemeral=True)
+        else:
+            await interaction.response.send_message(content=f"It looks like `{interaction.user.name}` is not a *lover* user.", ephemeral=True)
 
+    @love_kinks.command(name="list", description="List all your Kinks.")
+    async def love_kink_list(self, interaction: discord.Interaction):
+        lover = await LoverEntry.get_or_none(discord_id=interaction.user.id)
+        if lover is not None:
+            kinks = await lover.list_kinks()
+            if kinks is not None:
+                kink_embed = discord.Embed(title=f"{interaction.user.name} Kinks list.", color=interaction.user.color, timestamp=discord.utils.utcnow())
+                kink_embed.set_thumbnail(url=None if interaction.user.avatar == None else interaction.user.avatar.url)
+                for entry in kinks:
+                    kink_embed.add_field(name=entry["name"], value=entry["description"], inline=False)
+                await interaction.response.send_message(embed=kink_embed, ephemeral=True)
+            else:
+                await interaction.response.send_message(content=f"It looks like you do not have any Kinks, consider adding some? *cracks whip*", ephemeral=True)
+        else:
+            await interaction.response.send_message(content=f"It looks like `{interaction.user.name}` is not a *lover* user.", ephemeral=True)
+
+    @love_kinks.command(name="remove", description="Remove a Kink.")
+    @app_commands.autocomplete(kink=kinks_autocomplete)
+    async def love_kink_remove(self, interaction: discord.Interaction, kink: str):
+        lover = await LoverEntry.get_or_none(discord_id=interaction.user.id)
+        if lover is not None:
+            await lover.remove_kink(name=kink)
+            await interaction.response.send_message(content=f"Aww no longer into `{kink}` anymore? If that changes just add it back.", ephemeral=True)
+        else:
+            await interaction.response.send_message(content=f"It looks like `{interaction.user.name}` is not a *lover* user.", ephemeral=True)
     # @tasks.loop(time=datetime.time(hour=14, tzinfo=timezone.utc))
     # async def love_message_loop() -> None:
     #     print()
